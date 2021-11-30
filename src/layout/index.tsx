12f51d5361ae2a7164482eb7ddc147c2b5a0765d
Ozkan Canli <zkan.canl@gmail.com>
2021-11-30 22:42:25 +0100

    Implement file structure-2

---
+++ src/layout/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Header from './Header/Header';
+
+const Layout = (WrappedComponent: React.FunctionComponent) => () => (
+  <>
+    <Header />
+    <WrappedComponent />
+  </>
+);
+
+export default Layout;
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../../layout';
+
+const Home = () => {
+    return (
+        <div>
+            Home
+        </div>
+    )
+}
+
+export default Layout(Home);
